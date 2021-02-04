274d78f6c0386980a7a00102cf0f285b092bf3d6
Gipria <giovana_alves100@live.com>
2021-02-03 21:19:24 -0300

    iniciando js ,

---
+++ scripts.js
@@ -0,0 +1,40 @@
+const { totalmem } = require("os")
+
+const Modal= {
+    open(){
+        //Abrir modal
+        //Adicionar a class active ao modal
+        document
+        .querySelector('.modal-overlay')
+        .classList
+        .add('active')
+
+    },
+    close(){
+        //fechar o modal
+        //remover a class active do modal
+        document
+        .querySelector('.modal-overlay')
+        .classList
+        .remove('active')
+    }
+}
+
+const transactions = [{
+    id:1,
+    description:'Luz',
+    amount: -50000
+
+},{},{}]
+
+const Transaction = {
+    incomes() {
+        //somar as entradas
+    },
+    expenses() {
+        //somar as saídas
+    },
+    totalmem() {
+        //entradas - saídas
+    }
+}
